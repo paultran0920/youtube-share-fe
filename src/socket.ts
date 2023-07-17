90d56814e6107063dfe95d7cdbd21c77d0c716bc
Paul Tran <paul.tran.0920@gmail.com>
2023-07-17 10:24:33 +0700

    database migration and code refactor

--- src/socket.ts
+++ src/socket.ts
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 import { logInfo } from "./logs/logger";
 
-logInfo(`Socket server: ${process.env.REACT_APP_WS_URL ?? ""}`);
-export const appSocket = io(process.env.REACT_APP_WS_URL ?? "", {
+logInfo(`Socket server: ${process.env.REACT_APP_BE_ROOT ?? ""}`);
+export const appSocket = io(process.env.REACT_APP_BE_ROOT ?? "", {
   autoConnect: false,
 });
